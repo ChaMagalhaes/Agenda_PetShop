8b8c72e2afbfabd65cbf150009c697b135786268
Charles Silva <charlesreismagalhaes@outlook.com>
2026-09-09 21:19:13 -0300

    feat: Adicao da funcionalidade de cadastrar pet

--- src/main/java/com/github/chamagalhaes/petshop/controller/PetController.java
+++ src/main/java/com/github/chamagalhaes/petshop/controller/PetController.java
@@ -1,10 +1,13 @@
 package com.github.chamagalhaes.petshop.controller;
 
+import com.github.chamagalhaes.petshop.model.Pet;
 import com.github.chamagalhaes.petshop.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PetController {
@@ -15,5 +18,17 @@ public class PetController {
     public String index(Model model){
         model.addAttribute("petsList", petService.getAllPets());
         return "/petshop/index";
+    }
+
+    @GetMapping("/pet/create")
+    public String create(Model model) {
+        model.addAttribute("pet", new Pet());
+        return "petshop/create";
+    }
+
+    @PostMapping("/pet/save")
+    public String save (@ModelAttribute("pet") Pet pet) {
+        petService.savePet(pet);
+        return "redirect:/pet";
     }
 }
